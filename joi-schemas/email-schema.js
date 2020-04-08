926c0063ddba10eb197e04c0e3b3094c3453f229
Igor Kirpinev <ikirpinev@gmail.com>
2020-04-08 18:12:24 +0300

    Добавил еще кастомных сообщений для joi схемы email

--- joi-schemas/email-schema.js
+++ joi-schemas/email-schema.js
@@ -1,6 +1,8 @@
 const { Joi } = require('celebrate');
+const { messages } = require('../utils');
 
 module.exports.emailSchema = Joi.string().required().email().messages({
-  'any.required': 'Поле почты обязательно',
-  'string.email': 'Почта должна быть валидной',
+  'string.empty': messages.registration.email.isEmpty,
+  'any.required': messages.registration.email.isRequired,
+  'string.email': messages.registration.email.isInvalid,
 });
