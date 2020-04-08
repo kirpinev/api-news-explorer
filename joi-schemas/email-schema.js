65fe34f989d1edd55faaeab77ec23741011e6f22
Igor Kirpinev <ikirpinev@gmail.com>
2020-04-08 17:43:52 +0300

    Добавил кастомное описание ошибки для email в схеме joi

--- joi-schemas/email-schema.js
+++ joi-schemas/email-schema.js
@@ -1,3 +1,6 @@
 const { Joi } = require('celebrate');
 
-module.exports.emailSchema = Joi.string().required().email();
+module.exports.emailSchema = Joi.string().required().email().messages({
+  'any.required': 'Поле почты обязательно',
+  'string.email': 'Почта должна быть валидной',
+});
