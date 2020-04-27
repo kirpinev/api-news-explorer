ab50ea6555b4ba1c17f215e09088acaa202d2cda
Igor Kirpinev <ikirpinev@gmail.com>
2020-04-27 15:36:07 +0300

    Обновил CORS настройки

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,17 +1,22 @@
 const router = require('express').Router();
 const cors = require('cors');
 
-const whiteList = ['http://localhost:8080/'];
+// const whiteList = ['http://localhost:8080/'];
+//
+// const corsOptions = {
+//   origin(origin, callback) {
+//     if (whiteList.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+// };
 
 const corsOptions = {
-  origin(origin, callback) {
-    if (whiteList.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
+  origin: 'http://localhost:8080/',
+  optionsSuccessStatus: 200
+}
 
 router.use(cors(corsOptions));
 
