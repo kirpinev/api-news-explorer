ffab7d2f3023d0ca5cd7226bebf7f6c39cb3db86
Igor Kirpinev <ikirpinev@gmail.com>
2020-04-27 16:56:34 +0300

    Обновил CORS настройки

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,26 +1,12 @@
 const router = require('express').Router();
+const cors = require('cors');
 
-const whiteList = ['localhost:8080'];
+const corsOptions = {
+  origin: 'http://localhost:8080',
+  optionsSuccessStatus: 200,
+  credentials: true
+}
 
-const cors = (req, res, next) => {
-  const { origin } = req.headers;
-
-  if (whiteList.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization'
-    );
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET,HEAD,PUT,PATCH,POST,DELETE'
-    );
-  }
-
-  next();
-};
-
-router.use(cors);
+router.use(cors(corsOptions));
 
 module.exports = router;
