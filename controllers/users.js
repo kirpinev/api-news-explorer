33595466e1d541454b67424867c816766644dda4
Igor Kirpinev <ikirpinev@gmail.com>
2020-04-30 17:45:07 +0300

    Обновил cors

--- controllers/users.js
+++ controllers/users.js
@@ -32,7 +32,7 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        // sameSite: true,
+        sameSite: true,
       });
 
       res.status(200).send({ message: messages.authorization.isSuccessful });
