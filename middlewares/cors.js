38829650dd83268d0d6069528321087a4bc85c7e
Igor Kirpinev <ikirpinev@gmail.com>
2020-05-02 15:02:51 +0300

    Добавил статусы ошибок в тело ответа

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,33 +1,33 @@
-// const router = require('express').Router();
-// const cors = require('cors');
+const router = require('express').Router();
+const cors = require('cors');
+
+const corsOptions = {
+  origin: 'http://localhost:8080',
+  optionsSuccessStatus: 200,
+  credentials: true
+}
+
+router.use(cors(corsOptions));
+
+module.exports = router;
+
+// const allowedCors = ['http://localhost:8080', 'https://kirpinev.github.io'];
 //
-// const corsOptions = {
-//   origin: 'http://localhost:8080',
-//   optionsSuccessStatus: 200,
-//   credentials: true
-// }
+// module.exports.cors = (req, res, next) => {
+//   const { origin } = req.headers;
 //
-// router.use(cors(corsOptions));
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//     res.header('Access-Control-Allow-Credentials', true);
+//     res.header(
+//       'Access-Control-Allow-Headers',
+//       'Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization'
+//     );
+//     res.header(
+//       'Access-Control-Allow-Methods',
+//       'GET,HEAD,PUT,PATCH,POST,DELETE'
+//     );
+//   }
 //
-// module.exports = router;
-
-const allowedCors = ['http://localhost:8080', 'https://kirpinev.github.io'];
-
-module.exports.cors = (req, res, next) => {
-  const { origin } = req.headers;
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization'
-    );
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET,HEAD,PUT,PATCH,POST,DELETE'
-    );
-  }
-
-  next();
-};
+//   next();
+// };
