b3129fdc90264b7d8dcf76f8a6cea50381d3383d
Igor Kirpinev <ikirpinev@gmail.com>
2020-05-10 18:18:11 +0300

    Обновил cors настройки

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const cors = require('cors');
 
 const corsOptions = {
-  origin: 'http://localhost:8080',
+  origin: ['http://localhost:8080', 'https://kirpinev.github.io'],
   optionsSuccessStatus: 200,
   credentials: true
 }
