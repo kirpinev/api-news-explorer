952c996e7d8b276d3fb11fa2c497683886891c39
Igor Kirpinev <kirpinev@yahoo.com>
2021-01-08 13:51:42 +0300

    Обновил сервер

--- utils/database.js
+++ utils/database.js
@@ -1,9 +1,11 @@
 const { NODE_ENV, DATABASE_URL } = process.env;
 
-module.exports.dataBaseUrl = NODE_ENV === 'production' ? DATABASE_URL : 'mongodb://localhost:27017/newsdb';
+module.exports.dataBaseUrl =
+  NODE_ENV === 'production' ? DATABASE_URL : 'mongodb://localhost:27017/newsdb';
 
 module.exports.dataBaseOptions = {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
+  useUnifiedTopology: true,
 };
